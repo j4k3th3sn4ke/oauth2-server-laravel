7274d6bb3a12287b9a29fa1234c3fa46d6b01ce3
j4k3th3sn4ke <jtsm90401150@yahoo.com>
2018-01-10 15:49:08 -0600

    Update OAuth2ServerServiceProvider.php

--- src/OAuth2ServerServiceProvider.php
+++ src/OAuth2ServerServiceProvider.php
@@ -123,12 +123,11 @@ class OAuth2ServerServiceProvider extends ServiceProvider
                 $grant = $app->make($grantParams['class']);
                 $grant->setAccessTokenTTL($grantParams['access_token_ttl']);
 
-                if (array_key_exists('callback', $grantParams)) {
-                    list($className, $method) = array_pad(explode('@', $grantParams['callback']), 2, 'verify');
-                    Log::info('grantParams', ['grant' => $grantParams['callback']]);
-                    $verifier = $app->make($className);
-                    $grant->setVerifyCredentialsCallback([$verifier, $method]);
-                }
+                //if (array_key_exists('callback', $grantParams)) {
+                //    list($className, $method) = array_pad(explode('@', $grantParams['callback']), 2, 'verify');
+                //    $verifier = $app->make($className);
+                //    $grant->setVerifyCredentialsCallback([$verifier, $method]);
+                //}
 
                 if (array_key_exists('auth_token_ttl', $grantParams)) {
                     $grant->setAuthTokenTTL($grantParams['auth_token_ttl']);
